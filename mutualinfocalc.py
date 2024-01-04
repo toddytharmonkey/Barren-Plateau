b7a3c5ef1fab3c28862f37f3c406ec2190e8739f
Sonny Rappaport <str36@cornell.edu>
2024-01-03 22:24:39 -0500

    feat: now vary qubit count

--- mutualinfocalc.py
+++ mutualinfocalc.py
@@ -11,37 +11,22 @@ if __name__ == "__main__":
 
     n_a = 100
     n_p = 100
-    n_qubits = 6
+    qubit_list = [4,8,10,12,14,16]
     n_layers = 60
     probs = [.1]
 
-    for p in probs:
-        if p == 0:
-            layered_results = mutual_information_only_parameters(
-                n_qubits, n_layers, n_a, measurements=None
-            )
-            np.save(f"{n_qubits}_{p}_layeredresults", layered_results)
+    for n_qubits in qubit_list: 
+        for p in probs:
+            if p == 0:
+                layered_results = mutual_information_only_parameters(
+                    n_qubits, n_layers, n_a, measurements=None
+                )
+                np.save(f"{n_qubits}_{p}_layeredresults", layered_results)
 
-        else:
-            layered_results = mutual_info_different_measurements(
-                n_qubits, n_layers, n_a, n_p, p
-            )
-            np.save(f"{n_qubits}_{p}_layeredresults", layered_results)
+            else:
+                layered_results = mutual_info_different_measurements(
+                    n_qubits, n_layers, n_a, n_p, p
+                )
+                np.save(f"{n_qubits}_{p}_layeredresults", layered_results)
 
     client.close()
-
-    # #np.save("layered_resultstest2", np.array(layered_results))
-
-    # #aware
-    # print("final results")
-    # print(layered_results)
-    # plt.plot(layered_results[0])
-    # plt.xlabel('layers')
-    # plt.ylabel('mutual information')
-    # plt.show()
-
-    # #unaware
-    # plt.plot(layered_results[1])
-    # plt.xlabel('layers')
-    # plt.ylabel('mutual information')
-    # plt.show()
