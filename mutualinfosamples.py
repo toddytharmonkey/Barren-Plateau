5637f4f9a02e495b033df8d09f77247b2a562c48
Sonny Rappaport <str36@cornell.edu>
2024-01-05 15:21:32 -0500

    add script to just generate samples

---
+++ mutualinfosamples.py
@@ -0,0 +1,61 @@
+from MILT_mutual_information import *
+
+"""
+This version of the code calculates mutual info, first averaged over n_a thetas, then averaged over n_a different measurement configurations. 
+"""
+
+if __name__ == "__main__":
+    client = Client()
+
+    print("Dask Dashboard URL:", client.dashboard_link)
+
+    n_a = 100
+    n_p = 100
+    qubit_list = [12,14,16]
+    n_layers = 60
+    probs = [.05,.1,.2]
+
+    for n_qubits in qubit_list:
+        for p in probs:
+              p_i_m_given_thetas = generate_mutual_info_samples_dask_change_all_parameters_and_measurements(n_qubits, n_layers, n_a, n_p, p)
+
+              np.save(f"{n_qubits}_{p}_layeredresults_samples", p_i_m_given_thetas)
+
+    n_a = 100
+    n_p = 100
+    qubit_list = [4,6,8,10]
+    n_layers = 60
+    probs = [.05,.1,.2]
+
+    for n_qubits in qubit_list:
+        for p in probs:
+              p_i_m_given_thetas = generate_mutual_info_samples_dask_change_all_parameters_and_measurements(n_qubits, n_layers, n_a, n_p, p)
+
+              np.save(f"{n_qubits}_{p}_layeredresults_samples", p_i_m_given_thetas)
+
+    n_a = 100
+    n_p = 100
+    qubit_list = [4,6,8,10,12,14,16]
+    n_layers = 60
+    probs = [.3,.5,.7,.9]
+
+    for n_qubits in qubit_list:
+        for p in probs:
+              p_i_m_given_thetas = generate_mutual_info_samples_dask_change_all_parameters_and_measurements(n_qubits, n_layers, n_a, n_p, p)
+
+              np.save(f"{n_qubits}_{p}_layeredresults_samples", p_i_m_given_thetas)
+
+    n_a = 100
+    n_p = 100
+    qubit_list = [12,14,16]
+    n_layers = 60
+    probs = [.3,.5,.7,.9]
+
+    for n_qubits in qubit_list:
+        for p in probs:
+              p_i_m_given_thetas = generate_mutual_info_samples_dask_change_all_parameters_and_measurements(n_qubits, n_layers, n_a, n_p, p)
+
+              np.save(f"{n_qubits}_{p}_layeredresults_samples", p_i_m_given_thetas)
+
+
+    client.close()
