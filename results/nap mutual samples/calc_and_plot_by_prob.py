53fa506d9e60d422c32fe5c7911be00e08a5cf39
toddytharmonkey <s.rappaportsshs@gmail.com>
2024-05-03 21:59:43 -0400

    fix summary file generation, now plot using summary file

--- results/nap mutual samples/calc_and_plot_by_prob.py
+++ results/nap mutual samples/calc_and_plot_by_prob.py
@@ -12,54 +12,28 @@ This version of the code calculates and plots mutual info from samples.
 
 if __name__ == "__main__":
     n_ap = 1000
-    qubits = [4,6,8,10,12,14,16]
+    qubits = [4,6,8,10,12,14,16,18]
     n_layers = 60
-    probs = [0,.01,.05,.1,.2,.3,.5,.7,.9]
+    probs = [0,.05,.1,.2,.3,.5,.7,.9]
+
+    # results are in shape (num_qubits, num_prob,n_layers,2)
+    results = np.load("aggregated_data.npy")
 
     for i, n_qubits in enumerate(qubits):
 
         results_for_each_p = []
         er_each_p = []
 
-        examined_layer = 2
-        print("examined layer", examined_layer)
+        examined_layer = 2*n_qubits
 
         for j, p in enumerate(probs):
-            # Load your data based on n_qubits and p
-            # TODO: Rename data files to make this less horrific 
-            if p in [.01] and n_qubits in [10,12,14]:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_nap_10000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            elif p in [.01] and n_qubits not in [10,12,14]:
-                mean, error = np.full(n_layers,np.nan), np.full(n_layers,np.nan)
-            elif p in [.005] and n_qubits in [14]: 
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_nap_10000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            elif p in [.005] and n_qubits not in [14]: 
-                mean, error = np.full(n_layers,np.nan), np.full(n_layers,np.nan)
-            elif p in [0, .05, .1] and n_qubits in [12,14,16]:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_nap_10000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            elif p in [.2,.3,.5] and n_qubits == 16:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_nap_10000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            elif n_qubits == 10:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_nap_10000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            elif p == 0:
-                mean, error = np.load(f"{n_qubits}_{p}_layeredresults.npy")
-            elif n_qubits == 12 or n_qubits == 14:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_changeboth_1000.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
-            else:
-                p_i_m_given_thetas = np.load(f"{n_qubits}_{p}_layeredresults_samples_changeboth.npy")
-                mean, error = mutual_info_standard_error(p_i_m_given_thetas)
 
+            mean = results[i,j,:,0]
+            error = results[i,j,:,1]
+            
+            # Load your data based on n_qubits and p
             results_for_each_p.append(mean[examined_layer])
             er_each_p.append(error[examined_layer])
-
-        print(probs)
-        print(results_for_each_p)
 
         plt.errorbar(x=probs, y=results_for_each_p, yerr = er_each_p, label = f"{n_qubits}", marker='.')
 
--- results/nap mutual samples/generate_summary_file.py
+++ results/nap mutual samples/generate_summary_file.py
@@ -8,7 +8,7 @@ from MILT_mutual_information import *
 
 def load_and_aggregate_data(directory):
     # Parameters
-    num_qubits_values = list(range(4, 17, 2))
+    num_qubits_values = list(range(4, 20, 2))
     p_values = [0, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9]
     n_layers = 60
     nap_values = [10000, 1000]  # Order matters for preference
@@ -19,14 +19,14 @@ def load_and_aggregate_data(directory):
     )  # 2 for mean and error
     aggregated_data.fill(np.nan)  # Fill with NaNs to indicate missing data
 
-    # Iterate over all combinations of num_qubits, p_values, and nap_values
+        # Iterate over all combinations of num_qubits, p_values, and nap_values
     for i, num_qubits in enumerate(num_qubits_values):
         for j, p_value in enumerate(p_values):
             file_found = False
             for nap in nap_values:
                 if file_found:
                     break
-                if p_value == 0:
+                if p_value == 0 and num_qubits < 18:
                     mean, error = np.load(f"{num_qubits}_0_layeredresults.npy")
                 else:
                     filename = f"{num_qubits}_{p_value}_{n_layers}layers_nap_{nap}.npy"
@@ -34,15 +34,18 @@ def load_and_aggregate_data(directory):
                     path = os.path.join(directory, filename)
                     if os.path.isfile(path):
                         file_data = np.load(path)
-
-                        # Calculate mean and error across the axis corresponding to different samples (assuming axis 0)
+                        file_found = True 
                         mean, error = mutual_info_standard_error(file_data)
+                    else:
+                        continue
+                        # Calculate mean and error across the axis corresponding to different samples (assuming axis 0)
+                        #
+                print(error[2])
+                print(mean[2])
                 # Store the results in the array
                 aggregated_data[i, j, :, 0] = mean  # Storing mean
                 aggregated_data[i, j, :, 1] = error  # Storing error
-                file_found = (
-                    True  # Mark as file processed to skip 1000 if 10000 was found
-                )
+
     # Check for any NaN values in the aggregated data
     if np.isnan(aggregated_data).any():
         # Create a detailed error report
