abef1a32f3e9640d0303da9b224f94235f0dd8b1
toddytharmonkey <s.rappaportsshs@gmail.com>
2024-05-21 23:24:34 -0400

    actually save new samples post combination

--- results/nap mutual samples/combine_samples.py
+++ results/nap mutual samples/combine_samples.py
@@ -5,8 +5,10 @@ import numpy as np
 if __name__ == "__main__":
     for q in range(4,18,2):
         for p in [.22,.24,.26,.28]:
-            samples = np.load(f"{q}_{p}_layeredresults_samples_nap_10000") 
-            samples.append(np.load(f"{q}_{p}_layeredresults_samples_nap_10000_secondbatch"))
+            samples = np.load(f"{q}_{p}_layeredresults_samples_nap_10000.npy") 
+
+            print(samples.shape)
+            samples = np.append(samples, np.load(f"{q}_{p}_layeredresults_samples_nap_10000_secondbatch.npy"))
 
             np.save(f"{q}_{p}_layeredresults_samples_nap_10000",samples, )
     
