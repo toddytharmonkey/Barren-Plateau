10eac5cade0a669e2a784da15888bb9c1205c7e9
toddytharmonkey <s.rappaportsshs@gmail.com>
2024-05-21 23:21:46 -0400

    create script to combine samples

---
+++ results/nap mutual samples/combine_samples.py
@@ -0,0 +1,12 @@
+import numpy as np 
+# 8_0.28_layeredresults_samples_nap_10000.npy
+# 8_0.28_layeredresults_samples_nap_10000_secondbatch.npy
+
+if __name__ == "__main__":
+    for q in range(4,18,2):
+        for p in [.22,.24,.26,.28]:
+            samples = np.load(f"{q}_{p}_layeredresults_samples_nap_10000") 
+            samples.append(np.load(f"{q}_{p}_layeredresults_samples_nap_10000_secondbatch"))
+
+            np.save(f"{q}_{p}_layeredresults_samples_nap_10000",samples, )
+    
